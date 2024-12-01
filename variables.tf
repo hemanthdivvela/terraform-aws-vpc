1f5672419d8678d413c1048f5c66abbcd97eb098
hemanthdivvela <divvelahemanth7@gmail.com>
2024-12-01 16:36:43 +0530

    vpc

---
+++ variables.tf
@@ -0,0 +1,135 @@
+variable "project_name" {
+    type = string 
+
+}
+
+variable "environment" {
+    type = string 
+    default = "dev"
+}
+
+variable "common_tags"{
+    type = map
+}
+
+
+### vpc ####
+variable vpc_cidr {
+  type        = string
+  default     = "10.0.0.0/16"
+  
+}
+
+variable "enable_dns_hostnames" {
+    type = bool
+    default = true
+
+}
+
+variable "vpc_tags" {
+    type = map 
+    default = {}
+}
+
+###  IGW  ###
+
+variable "igw_tags" {
+    type = map 
+    default = {}
+}
+
+### public subnet ###
+variable "public_subnet_cidrs"{
+    type = list
+    validation {
+        condition = length(var.public_subnet_cidrs) == 2
+        error_message = "please provide 2 valid public subnet providers"
+    }
+
+}
+
+variable "public_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+### private subnet ###
+variable "private_subnet_cidrs"{
+    type = list
+    validation {
+        condition = length(var.private_subnet_cidrs) == 2
+        error_message = "please provide 2 valid private subnet providers"
+    }
+
+}
+
+variable "private_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+
+### database subnet ###
+variable "database_subnet_cidrs"{
+    type = list
+    validation {
+        condition = length(var.database_subnet_cidrs) == 2
+        error_message = "please provide 2 valid database subnet providers"
+    }
+
+}
+
+variable "database_subnet_group_tags" {
+    type = map
+    default = {}
+  
+}
+
+variable "database_subnet_cidr_tags" {
+    type = map
+    default = {}
+}
+
+
+#### natgateway ##########
+variable "nat_gateway_tags"{
+    type = map
+    default = {}
+}
+
+
+### public route table ###
+variable "public_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    type = map 
+    default = {}
+}
+
+
+
+##### peering ####
+variable "is_peering_required"{
+    type= bool
+    default = false
+
+}
+
+variable "acceptor_vpc_id"{
+    type = string
+    default = ""
+}
+
+variable "vpc_peering_tags" {
+    type = map 
+    default = {}
+  
+}
